2e8c06b599e21f89a7595d57b04375a54fc81f23
Ivan_HAN <hyf1996@qq.com>
2020-10-11 09:05:51 +0800

    11

--- config.py
+++ config.py
@@ -2,19 +2,19 @@ DATABASE = "./sql_app.db"
 
 
 PUBLIC_PATH = '/var/www/html'
-SETU_API = 'https://api.lolicon.app/setu/?apikey=429636915f81972ea69518'
+SETU_API = 'https://api.lolicon.app/setu/'
 MIRAI_API = 'http://127.0.0.1:8080'
 MIRAI_WS = 'ws://127.0.0.1:8080'
-QQ_NUM = 3469550651
-TARGET_NUM = [658248270]
+QQ_NUM = 123445678
+TARGET_NUM = [123456767]
 NICKNAME = '小右'
 SLEEP_TIME = 30000
-MIRAI_KEY = 'INITKEYiCVswvf9'
+MIRAI_KEY = '123333'
 
 TEMP_MAX = 9999
 
 
 TARGET = [{
-    'qq': 3469550651,
-    'group': 658248270
+    'qq': 1233333,
+    'group': 12341234
 }]
