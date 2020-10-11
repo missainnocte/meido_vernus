77c5782d7c9a362995b411eca975fdf58a963fcf
Ivan_HAN <hyf1996@qq.com>
2020-10-11 12:32:00 +0800

    修改缓存

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import logging as log
+import os
 
 import requests
 from requests.exceptions import HTTPError
@@ -20,6 +21,7 @@ def mirai_of(r: requests.Response):
         raise Exception
     return res
 
+
 def localize(url):
     # if id == TEMP_MAX:
     #     id = 0
@@ -30,11 +32,19 @@ def localize(url):
     filename = url.split('/')[-1]
     path = '/temp/{}'.format(filename)
     log.debug('缓存文件下载完成, url: {}, path: {}'.format(url, path))
-    with open('{}{}'.format(PUBLIC_PATH, path), 'wb') as f:
-        f.write(r.content)
+    try:
+        p = os.path.join(PUBLIC_PATH, path)
+        if not os.path.exists(p):
+            os.makedirs(p)
+        with open(p, 'wb') as f:
+            f.write(r.content)
+    except Exception as e:
+        log.error(e)
     log.debug('完成缓存文件, url: {}, local: {}'.format(url, path))
     return (path, id)
 
+
 if __name__ == "__main__":
     for r in range(0, 10):
-        print(localize('https://i.pixiv.cat/img-original/img/2018/08/31/20/00/00/70474544_p0.png'))
+        print(localize(
+            'https://i.pixiv.cat/img-original/img/2018/08/31/20/00/00/70474544_p0.png'))
